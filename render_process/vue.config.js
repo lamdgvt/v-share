776b73955b9d1ac0aecb9fbaa65e4f8391703917
double <491801318@qq.com>
2024-02-10 18:00:02 +0800

    feature: init code

--- render_process/vue.config.js
+++ render_process/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  devServer: {
+    allowedHosts: 'all',
+    webSocketServer: false
+  },
   transpileDependencies: true,
 });
