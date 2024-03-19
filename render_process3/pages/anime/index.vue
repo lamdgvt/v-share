88c26f8c95c939e6351e0aab6155f541cf731df8
double <491801318@qq.com>
2024-03-19 23:37:17 +0800

    feature: update code

--- render_process3/pages/anime/index.vue
+++ render_process3/pages/anime/index.vue
@@ -1,23 +1,31 @@
 <script lang="ts" setup>
 
 const route = useRoute()
+const router = useRouter()
+
+// 打开其他番组
+const openScheduleEvent = (team: any) => {
+    router.push({ path: `/animeCalendar/${team.bgmId}`, })
+
+    console.log(team)
+}
 
 // 获取动漫季度信息
-// const getQuarterList = async () => await useRequest(`/api/share/anime/quarterList` || '') || {}
+const getQuarterList = async () => await useRequest(`/api/share/anime/quarterList` || '') || {}
 // 获取全部的季度信息
-// const getSchedule = async () => await useRequest('/api/share/anime/schedule') || {}
+const getSchedule = async () => await useRequest('/api/share/anime/schedule') || {}
 
 // 获取当前季度动漫
 const getScheduleAnime = async () => await useRequest('/api/share/anime/animeByBgmId', { method: 'post', body: { bgmId: 'nowaday' } }) || {};
 
 // 初始化
-// const quarterSync: any = await getQuarterList()
-// const scheduleSync: any = await getSchedule()
+const quarterSync: any = await getQuarterList()
+const scheduleSync: any = await getSchedule()
 const scheduleAnimeSync: any = await getScheduleAnime()
 
 // 数据源
-// const schedule = computed(() => scheduleSync.data.value?.data || [])
-// const quarter = computed(() => quarterSync.data.value?.data || [])
+const schedule = computed(() => scheduleSync.data.value?.data || [])
+const quarter = computed(() => quarterSync.data.value?.data || [])
 const anime = computed<any>(() => scheduleAnimeSync.data.value?.data || {})
 
 
@@ -25,4 +33,20 @@ const anime = computed<any>(() => scheduleAnimeSync.data.value?.data || {})
 
 <template>
     <common-anime-calendar :record="anime" weekOrder />
+
+    <!-- 其他番组信息 -->
+    <common-bar title="其他番组信息">
+        <div class="grid grid-cols-4 gap-4">
+            <UButton v-for="team in schedule" :key="team.id" icon="i-heroicons-inbox" color="gray"
+                @click="openScheduleEvent(team)">
+                {{ team.bgmId }}
+            </UButton>
+        </div>
+    </common-bar>
 </template>
+
+<style lang="less" scoped>
+.schedule-layout {
+    display: grid;
+}
+</style>
