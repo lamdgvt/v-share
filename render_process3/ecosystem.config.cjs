4f7ff7d757181c460e7e581d16a134dcdb683239
double <491801318@qq.com>
2024-04-05 19:36:38 +0800

    feature: update port

--- render_process3/ecosystem.config.cjs
+++ render_process3/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "v-share",
-      port: "3000",
+      port: "3300",
       exec_mode: "cluster",
       instances: "max",
       script: ".output/server/index.mjs",
